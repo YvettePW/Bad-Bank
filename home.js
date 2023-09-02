acbd3273d1f679ea69426a91cca53e65a48725d6
YvettePW <130176245+YvettePW@users.noreply.github.com>
2023-09-01 22:06:50 -0700

    Add files via upload

--- home.js
+++ home.js
@@ -1,8 +1,12 @@
 function Home(){
-    const ctx = React.useContext(UserContext);
     return (
-        <h1>Home<br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card
+            bgcolor="primary"
+            txtcolor="white"
+            header="BadBank Landing Page"
+            title="Welcome to the bank"
+            text="You can use this bank"
+            body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+        />
     );
 }
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function Spa(){
     return (
         <HashRouter>
-        <NavBar/>
-        <UserContext.Provider value={{users:[{name:'abel', email:'abel@mit.edu',password:'secret',balance:100}]}}>
-            <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" component={CreateAccount} />    
-            <Route path="/alldata/" component={AllData} />   
-            <Route path="/balance/" component={Balance} />     
-            <Route path="/deposit/" component={Deposit} />     
-            <Route path="/withdraw/" component={Withdraw} />     
-            <Route path="/login/" component={Login} />
-        </UserContext.Provider>          
+            <NavBar/>
+            <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+                <Route path="/" exact component={Home} />
+                <Route path="/CreateAccount/" component={CreateAccount} />
+                <Route path="/alldata/" component={AllData} />
+            </UserContext.Provider>   
         </HashRouter>
     );
 }
